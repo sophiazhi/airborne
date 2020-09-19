2aa73c2fd35923540a8adb6ced11b3f088ee3b4c
Sophia Zhi <sophiazhi@gmail.com>
2020-09-19 13:00:13 -0400

    form frontend

--- src/Contribute.js
+++ src/Contribute.js
@@ -12,21 +12,30 @@ class Contribute extends React.Component {
         this.date = React.createRef();
         this.time = React.createRef();
         this.airline = React.createRef();
+        this.crowded = React.createRef();
+        this.safety = React.createRef();
+        this.comments = React.createRef();
 
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit(event) {
+        // require departure, arrival, date, time, airline
+        // possibly require crowded and safety?
+        // don't require comments
         alert('test submitted ' + this.departure.current.value 
                 + " to " + this.arrival.current.value
                 + " on " + this.date.current.value + ", " + this.time.current.value
-                + " with " + this.airline.current.value);
+                + " with " + this.airline.current.value
+                + " at " + this.safety.current.value + " safety and " + this.crowded.current.value + " crowded"
+                + this.comments.current.value);
         // actually need to save to database
     }
 
     render() {
         return (
             <Form className="body">
+                <h1>Share your flight experience</h1>
                 <Form.Row>
                     <Col>
                         <Form.Group controlId="formDeparture">
@@ -68,6 +77,31 @@ class Contribute extends React.Component {
                 <Form.Group controlId="formAirline">
                     <Form.Label>Airline</Form.Label>
                     <Form.Control ref={this.airline} type="text" placeholder="Company"/>
+                </Form.Group>
+                <Form.Row>
+                    <Col>No other passengers in sight</Col>
+                    <Col xs={8}>
+                        <Form.Group controlId="formCrowded">
+                            <Form.Label>How full was the flight?</Form.Label>
+                            <Form.Control ref={this.crowded} type="range" custom />
+                        </Form.Group>
+                    </Col>
+                    <Col>Every seat was filled</Col>
+                </Form.Row>
+                <Form.Row>
+                    <Col>If someone had coronavirus I definitely contracted it</Col>
+                    <Col xs={8}>
+                        <Form.Group controlId="formSafety">
+                            <Form.Label>How safe did you feel?</Form.Label>
+                            <Form.Control ref={this.safety} type="range" custom />
+                        </Form.Group>
+                    </Col>
+                    <Col>No contact with other people</Col>
+                </Form.Row>
+
+                <Form.Group controlId="formComments">
+                    <Form.Label>Other comments</Form.Label>
+                    <Form.Control ref={this.comments} type="text" placeholder="Thoughts" />
                 </Form.Group>
                 
                 
