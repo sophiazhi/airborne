ef8ec065d211bd4dd5d50f0abc8d9f8e60488fc1
Sophia Zhi <sophiazhi@gmail.com>
2020-09-19 02:48:06 -0400

    one form element on contribute page

--- src/Contribute.js
+++ src/Contribute.js
@@ -1,9 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class Contribute extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {departure: '',
+                        arrival: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({departure: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('test submitted ' + this.state.departure);
+        // actually need to save to database
+    }
+
     render() {
-        return <p>Contribute section</p>
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Departure:
+                    <input type="text" value={this.state.departure} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        )
     }
 }
 
